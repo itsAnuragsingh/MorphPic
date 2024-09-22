213e4026673d65d6315ae6738434c03eaba8be92
Anurag Singh <itsanurag707@gmail.com>
2024-09-22 11:49:37 +0530

    api updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://morphify-api.vercel.app/api';
 
 export const uploadFile = async (file) => {
   const formData = new FormData();
